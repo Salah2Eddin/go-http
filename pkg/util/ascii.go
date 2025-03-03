6628a0ffd0b866d1be81f11e43c107a07ec75bb5
salah2eddin <salaheddin.mohamed12345@gmail.com>
2025-03-03 15:09:05 +0200

    feat: comment to explaing the magic numbers in USASCII checks

--- pkg/util/ascii.go
+++ pkg/util/ascii.go
@@ -1,13 +1,22 @@
 package util
 
 func IsVisiableUSASCII(b byte) bool {
+	/*
+		VCHAR   =  %x21-7E  ; visible (printing) characters
+		RFC5234 B.1
+	*/
 	return b >= 0x21 && b <= 0x7E
 }
 
 func IsUSASCII(b byte) bool {
+	// USASCII ranges from 0 to 127 (dec) or 0 to 7F (hex)
 	return b <= 0x7F
 }
 
 func IsCTLCharASCII(b byte) bool {
+	/*
+		CTL =  %x00-1F / %x7F    ; controls
+		RFC5234 B.1
+	*/
 	return b <= 0x1F || b == 0x7F
 }
