f828b6f9f6de42d5aa90fa766e8608bdb7496625
salah2eddin <salaheddin.mohamed12345@gmail.com>
2025-03-07 06:36:13 +0200

    feat: add custom error types for route handling
    Introduce `ErrRouteExists` and `ErrRouteNotFound` types.

---
+++ pkg/errors/router.go
@@ -0,0 +1,19 @@
+package errors
+
+import "fmt"
+
+type ErrRouteExists struct {
+	Route string
+}
+
+func (err ErrRouteExists) Error() string {
+	return fmt.Sprintf("%s already exists", err.Route)
+}
+
+type ErrRouteNotFound struct {
+	Route string
+}
+
+func (err ErrRouteNotFound) Error() string {
+	return fmt.Sprintf("%s doesn't exist", err.Route)
+}
