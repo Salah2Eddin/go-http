afde7ba9db1d8579c7b6b070c13991efe9121db0
salah2eddin <salaheddin.mohamed12345@gmail.com>
2025-10-23 13:12:30 +0300

    refactor: removed unused String() and Byte() methods from StatusLine

--- pkg/response/line.go
+++ pkg/response/line.go
@@ -1,17 +1,7 @@
 package response
 
-import "fmt"
-
 type StatusLine struct {
 	Version string
 	Code    string
 	Phrase  string
-}
-
-func (status StatusLine) String() string {
-	return fmt.Sprintf("%s %s %s\r\n", status.Version, status.Code, status.Phrase)
-}
-
-func (status StatusLine) Bytes() []byte {
-	return []byte(status.String())
 }
