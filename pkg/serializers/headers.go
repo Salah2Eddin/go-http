97a621140a53207aa585a3289c9adfc56ce716ee
salah2eddin <salaheddin.mohamed12345@gmail.com>
2025-11-07 12:25:19 +0200

    fix: add missing CRLF after each serialized header

--- pkg/serializers/headers.go
+++ pkg/serializers/headers.go
@@ -12,6 +12,7 @@ type HeadersSerializer struct {
 func (h HeadersSerializer) Serialize(headers *httpheaders.Headers, buf *bytes.Buffer) {
 	for _, header := range headers.Headers() {
 		h.headerSerializer.Serialize(header, buf)
+		buf.WriteString("\r\n")
 	}
 	buf.WriteString("\r\n")
 }
